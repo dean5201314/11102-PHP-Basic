30d84db9cb48f9bbe701e06037c0148a32e8ef23
dean5201314 <smile90624@gmail.com>
2023-10-06 09:25:49 +0800

    echo loop result

--- loop.php
+++ loop.php
@@ -1,8 +1,8 @@
 <?php 
 $sum=0;
 for ($i=1;$i<=100;$i++){
-    echo "sum=".$sum."&nbsp;&nbsp;"."i=".$i."&nbsp;&nbsp;";
-    $sum=$sum+$i;   
-    echo "sum+i=".$sum."<br>";
+    echo '$sum='.$sum.'&nbsp;&nbsp;'.'$i='.$i.'&nbsp;&nbsp;';
+    $sum+=$i;   
+    echo '$sum+$i='.$sum.'<br>';
 }
 ?>
