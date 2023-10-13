6e23ce4378ae3d67cf37357049e7bcd898be8008
dean5201314 <smile90624@gmail.com>
2023-10-13 16:19:03 +0800

    test

--- pra04a.php
+++ pra04a.php
@@ -126,4 +126,23 @@ for($i=0;$i<7;$i++){
     }
     echo "<br>";
 }
+?>
+
+
+<h2>矩形+對角線</h2>
+<?php
+
+for($i=0;$i<7;$i++){
+    for($j=0;$j<7;$j++){
+        if($i==0 || $i==6){
+            echo "*";
+        }else if($j==0 || $j==6){
+            echo "*";
+        }else if($i==$j || $i+$j==6)
+        {
+            echo "&nbsp;";
+        }
+    }
+    echo "<br>";
+}
 
