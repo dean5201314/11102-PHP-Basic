72b32b21a73bffe5fc7da87b663d9b02dce57f11
dean5201314 <smile90624@gmail.com>
2023-10-23 09:05:23 +0800

    array exe 01

---
+++ array_fn.php
@@ -0,0 +1,21 @@
+<?php 
+
+$array=[3,2,7,10,30,17];
+
+echo "<pre>";
+print_r($array);
+echo "<pre>";
+
+sort($array);
+
+echo "<pre>";
+print_r($array);
+echo "<pre>";
+
+rsort($array);
+
+echo "<pre>";
+print_r($array);
+echo "<pre>";
+
+?>
