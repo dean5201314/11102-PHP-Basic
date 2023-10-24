0e6876f18bc949227e95a449ec658b7309090be3
dean5201314 <smile90624@gmail.com>
2023-10-24 11:17:49 +0800

    Date calc.

--- date.php
+++ date.php
@@ -64,3 +64,45 @@ echo date($today, 'm月d日 l')."<br>";
 echo date($today, 'm月d日 l')."<br>";
 */
 ?>
+
+
+<h2>利用迴圈來計算連續五個周一的日期</h2>
+<ul>
+    <li>1. 2021-10-04 星期一</li>
+    <li>2. 2021-10-11 星期一</li>
+    <li>3. 2021-10-18 星期一</li>
+    <li>4. 2021-10-25 星期一</li>
+    <li>5. 2021-11-01 星期一</li>
+</ul>
+
+<?php
+$todayWeek=date("N");
+// echo $todayWeek."<br>";
+// echo ($todayWeek-1)."<b>";
+
+$diff=1-$todayWeek;
+$lastMonday=strtotime("$diff days");
+// echo date("Y-m-d",$lastMonday);
+// echo "<br>";
+$nextMonday=date("Y-m-d",strtotime("+1 week",$lastMonday));
+
+
+for ($i=0;$i < 5;$i++) {     
+    $nextMonday=date("Y-m-d l",strtotime("+1 wwek",strtotime($nextMonday)));
+    echo $nextMonday;
+    echo "<br>";
+}
+?>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
